f51d326a16f7ee7a86fc67f7a1162e486c164515
Tatiana <tatisalexa16@gmail.com>
2025-04-03 08:31:34 -0500

    Inicializacion del projecto

---
+++ src/main/java/com/mycompany/crud2_mysql/Crud2_mysql.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.crud2_mysql;
+
+/**
+ *
+ * @author Personal
+ */
+public class Crud2_mysql {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
